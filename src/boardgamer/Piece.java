4bc655fe257a770570183e48ea237eb64651f8ca
Uillianleal <willianleal52@hotmail.com>
2021-08-18 13:36:29 -0300

    Starting to implement Board and Piece

---
+++ src/boardgamer/Piece.java
@@ -0,0 +1,24 @@
+package boardgamer;
+
+public class Piece {
+	
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+	
+	
+
+
+	
+	
+	
+	
+
+}
