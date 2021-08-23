036382f70a62399a7e7df796a8dc083b413dde0b
Uillianleal <willianleal52@hotmail.com>
2021-08-23 18:05:16 -0300

    Printing possible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,14 +8,14 @@ import chess.pieces.Rook;
 
 public class ChessMatch {
 	
-	private Board board;
+	private static Board board;
 	
 	public ChessMatch() {
 		board = new Board(8, 8);
 		initialSetup();
 	}
 	
-	public ChessPiece[][] getPieces(){
+	public static ChessPiece[][] getPieces(){
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColums()];
 		for(int i=0; i<board.getRows(); i++) {
 			for(int j=0; j<board.getColums(); j++) {
@@ -25,6 +25,12 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	public boolean[][] possibleMoves(ChessPosition sourcePosition){
+		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+		}
+			
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
