d2eb4ac880984c1d82d37dd310feb61545830599
Uillianleal <willianleal52@hotmail.com>
2021-08-26 17:59:01 -0300

    Special move - castling

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -8,7 +8,6 @@ public class ChessPosition {
 	private char column;
 	private int row;
 	
-	
 	public ChessPosition(char column, int row) {
 		if(column < 'a' || column > 'h' || row < 1 || row > 8 ) {
 			throw new BoardException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
@@ -17,11 +16,9 @@ public class ChessPosition {
 		this.row = row;
 	}
 
-
 	public char getColumn() {
 		return column;
 	}
-
 
 	public int getRow() {
 		return row;
